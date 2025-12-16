51b580fc3095a0259c7a6ef1527cb487e743faeb
Mark Voltaire <markjvoltaire@gmail.com>
2025-12-16 12:04:42 -0500

    starting to mesh

--- Screens/HomeScreen.jsx
+++ Screens/HomeScreen.jsx
@@ -190,7 +190,7 @@ export default function HomeScreen() {
   };
 
   const handleGamePress = (market) => {
-    navigation.navigate("Chart", { game: market });
+    navigation.navigate("MarketDetail", { game: market });
   };
 
   // Handle both single market object and array of markets
